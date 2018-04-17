5e3801193b9335828a922e3db9888bda0e7bd7d3
guiyin.xiong <guiyin.xiong@gmail.cm>
2018-04-17 14:26:57 +0800

    haystack全文索引

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 """自定义Django模板标签："""
-from ..models import Post, Category
+from ..models import Post, Category,Tag
 from django import template
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -17,5 +18,9 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    # return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,11 @@ from .models import Post, Tag, Category
 import markdown
 from comments.forms import CommentForm
 from django.views.generic import ListView,DetailView
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from django.db.models import Q
 
 # Create your views here.
-
 
 # def index(request):
 #     return render(request,'blog/base.html',context={'title': '我的博客首页', 'welcome': '欢迎访问我的博客首页'})
@@ -25,6 +27,68 @@ class IndexView(ListView):
     # 指定paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章；
     paginate_by = 2
     # 如果不是用类视图，分页功能可以参考django.cor.paginator的相关文档；
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        pagination_data = self.pagination_data(paginator,page,is_paginated)
+        context.update(pagination_data)
+
+        return context
+
+    def pagination_data(self,paginator,page,is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages -1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number -3) > 0 else 0:page_number -1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number -3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last
+        }
+
+        return data
 
 
 def detail(request,pk):
@@ -101,13 +165,17 @@ class PostDetailView(DetailView):
 
     def get_object(self, queryset=None):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+
         post = super(PostDetailView, self).get_object(queryset=None)
-        post.body = markdown.markdown(post.body,
-                                      extensions=[
-                                          'markdown.extensions.extra',
-                                          'markdown.extensions.codehilite',
-                                          'markdown.extensions.toc',
-                                      ])
+        md = markdown.Markdown(extensions=[
+              'markdown.extensions.extra',
+              'markdown.extensions.codehilite',
+              # 需要在顶部引入TocExtension 和 slugify,
+              TocExtension(slugify=slugify)
+
+          ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
 
     def get_context_data(self, **kwargs):
@@ -147,3 +215,22 @@ class CategoryView(IndexView):
     def get_queryset(self):
         cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class TagView(IndexView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        return super(TagView,self).get_queryset().filter(tags=tag)
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = "请输入关键词"
+        return render(request,'blog/index.html',{'error_msg': error_msg})
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request,'blog/index.html',{'error_msg': error_msg,'post_list': post_list})
